1712b8637f5dd3516ac8d9be92160cc1dc3fdc47
Syed Raza <syed.raza011@gmail.com>
2023-06-19 17:00:11 -0400

    First Push

---
+++ Posts/PostService.swift
@@ -0,0 +1,8 @@
+//
+//  PostService.swift
+//  Posts
+//
+//  Created by Syed Raza on 6/19/23.
+//
+
+import Foundation
---
+++ Posts/SinglePostCell.swift
@@ -0,0 +1,20 @@
+//
+//  SinglePostCell.swift
+//  Posts
+//
+//  Created by Syed Raza on 6/19/23.
+//
+
+import SwiftUI
+
+struct SinglePostCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SinglePostCell_Previews: PreviewProvider {
+    static var previews: some View {
+        SinglePostCell()
+    }
+}
