421a40ad483b5f523ca5f98976e9bdae456ef7cd
Syed Raza <syed.raza011@gmail.com>
2023-06-19 16:58:16 -0400

    Initial Commit

---
+++ Posts/PostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsApp.swift
+//  Posts
+//
+//  Created by Syed Raza on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
