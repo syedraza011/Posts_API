472b19b38200bd0d2f6f22f12cf1493437ff9fb3
Syed Raza <syed.raza011@gmail.com>
2023-06-19 22:43:12 -0400

    added, viewModel, PostResponse and SinglePostCell

--- Posts/Views/ContentView.swift
+++ Posts/Views/ContentView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Syed Raza on 6/19/23.
 //
-
 import SwiftUI
 struct ContentView: View {
     @StateObject var viewModel = PostViewModel()
@@ -13,15 +12,15 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 List(viewModel.posts) { post in
-                    VStack(alignment: .leading, spacing: 4) {
-                        HStack {
+                    NavigationLink(destination: PostDetailsView(post: post)) {
+                        VStack(alignment: .leading, spacing: 4) {
                             Text("\(post.id)")
                                 .font(.subheadline)
                                 .foregroundColor(.black)
                             Text(post.title)
                                 .foregroundColor(.primary)
-                            Text(post.body)
-                                .foregroundColor(.primary)
+//                            Text(post.body)
+//                                .foregroundColor(.primary)
                         }
                     }
                     .padding()
@@ -35,39 +34,39 @@ struct ContentView: View {
     }
 }
 
+struct PostDetailsView: View {
+    let post: Post
+    
+    var body: some View {
+        VStack {
+            Text("Post Details")
+                .font(.title)
+                .foregroundColor(.primary)
+                .padding()
+            
+            Text("ID: \(post.id)")
+                .font(.subheadline)
+                .foregroundColor(.black)
+                .padding()
+            
+            Text("Title: \(post.title)")
+                .foregroundColor(.primary)
+                .padding()
+            
+            Text("Body: \(post.body)")
+                .foregroundColor(.primary)
+                .padding()
+            
+            Spacer()
+        }
+        .navigationTitle("Details")
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
 
-//
-//struct ContentView: View {
-//    @StateObject var viewModel = PostViewModel()
-//    var body: some View {
-////        NavigationView {
-////            VStack {
-//                List (viewModel.posts){ post in
-////                    VStack(alignment: .leading, spacing: 4) {
-////                        HStack {
-//                            Text("\(post.id)")
-//                                .font(.subheadline)
-//                                .foregroundColor(.black )
-//                            Text(post.title)
-//                                .foregroundColor(.primary)
-//                            Text(post.body)
-//                                .foregroundColor(.primary)
-//                        }
-//                    }
-////                    .padding()
-//                }
-////            }
-////        }
-////    }
-////}
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+
